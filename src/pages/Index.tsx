b6c1fd5cfdbbe532cd19dc16f99839456d53056a
EricDegaardt <123329634+EricDegaardt@users.noreply.github.com>
2025-06-24 12:36:03 +0200

    Change game aspect ratio to 2:3

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,8 +3,8 @@ import MobileOptimizedGame from "../components/game/MobileOptimizedGame";
 const Index = () => {
   return (
     <div className="min-h-screen bg-black flex items-center justify-center p-4">
-      {/* 4:5 aspect ratio for consistent mobile experience */}
-      <div className="w-full max-w-sm aspect-[4/5]">
+      {/* 2:3 aspect ratio for taller portrait experience */}
+      <div className="w-full max-w-sm aspect-[2/3]">
         <MobileOptimizedGame />
       </div>
     </div>
