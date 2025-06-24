87ed16a44ea4c684cd53f94f54d155e501f70661
EricDegaardt <123329634+EricDegaardt@users.noreply.github.com>
2025-06-24 03:39:24 +0200

    Remove animated stars from the sky

--- src/components/game/Skyline.tsx
+++ src/components/game/Skyline.tsx
@@ -1,13 +1,9 @@
-
 import React from 'react';
 import './Skyline.css';
 
 const Skyline = () => {
     return (
         <div className="parallax-bg">
-            <div id="stars1"></div>
-            <div id="stars2"></div>
-            <div id="stars3"></div>
             <div id="city-background"></div>
             <div id="city-far"></div>
             <div id="city-near"></div>
