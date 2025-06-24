6bb0b950c0fdb0a179a3cb58cff2ac84732bdce4
EricDegaardt <123329634+EricDegaardt@users.noreply.github.com>
2025-06-24 04:22:31 +0200

    Create subtle neon purple sky with white cloud stripes

--- src/components/game/Skyline.tsx
+++ src/components/game/Skyline.tsx
@@ -4,6 +4,15 @@ import './Skyline.css';
 const Skyline = () => {
     return (
         <div className="parallax-bg">
+            {/* Cloud stripes */}
+            <div className="cloud-stripe"></div>
+            <div className="cloud-stripe"></div>
+            <div className="cloud-stripe"></div>
+            <div className="cloud-stripe"></div>
+            <div className="cloud-stripe"></div>
+            <div className="cloud-stripe"></div>
+            
+            {/* City layers */}
             <div id="city-background"></div>
             <div id="city-far"></div>
             <div id="city-near"></div>
